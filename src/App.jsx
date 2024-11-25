bd5c44a5bcb1827dd0c9b86c09df6ab44b06499b
Name <user@example.com>
2024-11-25 11:29:27 +0700

    build(finishing 404notfound page)

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
 import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen h-screen">
-      <Vans />
+      <NotFound />
     </div>
   );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../component/Nav";
+import Footer from "../section/Footer";
+import Button from "../component/Button";
+
+function NotFound() {
+  return (
+    <div>
+      <Navbar />
+      <main className="h-screen bg-primary flex flex-col items-center justify-center px-4 sm:px-6 md:px-8">
+        <h1 className="font-inter text-2xl sm:text-3xl md:text-4xl leading-8 sm:leading-9 md:leading-10 font-bold text-hitam text-center mb-6 sm:mb-8 md:mb-[30px]">
+          Sorry, the page you were <br className="hidden sm:block" /> looking
+          for was not found.
+        </h1>
+        <Button variant="blacky" width="w-full sm:w-3/6 md:w-2/6">
+          Return to home
+        </Button>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default NotFound;
